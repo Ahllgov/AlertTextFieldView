bab7003948d1242cd865da80e0843c7e57e905da
Ahllgov <magaahilgov1@gmail.com>
2022-06-16 12:15:56 +0300

    Initial Commit

---
+++ CustomAlertView + TextField/ContentView.swift
@@ -0,0 +1,57 @@
+//
+//  ContentView.swift
+//  CustomAlertView + TextField
+//
+//  Created by Магомед Ахильгов on 16.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Button("Present Alert") {
+            alertTF(title: "Login", message: "Please enter your password for @Ahllgov", hintText: "12345", primaryTitle: "Login", secondaryTitle: "Cancel", primaryAction: { text in
+                print(text)
+            }, secondaryAction: {
+                print("Cancelled")
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+extension View {
+    func alertTF(title: String, message: String, hintText: String, primaryTitle: String, secondaryTitle: String, primaryAction: @escaping (String) -> (), secondaryAction: @escaping () -> ()) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { field  in
+            field.placeholder = hintText
+        }
+        alert.addAction(.init(title: secondaryTitle, style: .cancel, handler: { _ in
+            secondaryAction()
+        }))
+        alert.addAction(.init(title: primaryTitle, style: .default, handler: { _ in
+            if let text = alert.textFields?[0].text {
+                primaryAction(text)
+            } else {
+                primaryAction("")
+            }
+        }))
+        
+        rootController().present(alert, animated: true, completion: nil)
+    }
+    
+    func rootController() -> UIViewController {
+        guard let screen = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            return .init()
+        }
+        guard let root = screen.windows.first?.rootViewController else {
+            return .init()
+        }
+        return root
+    }
+}
